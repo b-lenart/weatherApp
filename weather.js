61cfcac2642b25c735966b853b70b9fcf8daa2c9
BartoszLenart <bartek9513@interia.pl>
2018-11-13 21:42:21 +0100

    time added

--- weather.js
+++ weather.js
@@ -3,27 +3,18 @@ var apiKey = 'a2e65ce31be330f009f5fd707405cd58';
 //3094802-Cracow, 5128638-New York, 3372783-Ponta Delgada, 2643741-City of London
 let url = 'http://api.openweathermap.org/data/2.5/group?id=2643741,3094802,5128638,3372783&units=metric&APPID=' + apiKey;
 
-// console.log(url);
-
 let requestWeather = new XMLHttpRequest();
 
 console.log(`URL: ${url}`);
 
 requestWeather.open('GET', url, true)
 requestWeather.send()
-// console.log();
 
 const w1 = document.querySelector('.weather-info1');
 
 const c1 = document.querySelector('.w-city1');
 const c2 = document.querySelector('.w-city2');
 const c3 = document.querySelector('.w-city3');
-
-const time = document.querySelector('#time');
-
-// function cityNameApi(cityName) {
-//     return 'http://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&units=metric&APPID=a2e65ce31be330f009f5fd707405cd58';
-// }
 
 requestWeather.addEventListener('readystatechange', function(e) {
     console.log(`ReadyState: ${e.target.readyState}`);
@@ -37,3 +28,20 @@ requestWeather.addEventListener('readystatechange', function(e) {
         c3.innerHTML = parseInt(rText.list[3].main.temp*10)/10 + ' &deg;C';
     }
 });
+
+//Time
+
+
+
+// console.log(`time: ${h}:${m}:${s}`);
+
+const timeCont = document.querySelector('#time');
+
+var timeInterval = setInterval(function() {
+    let date = new Date();
+    let h = date.getHours();
+    let m = date.getMinutes();
+    let s = date.getSeconds();
+    // console.log(`time: ${h}:${m}:${s}`);
+    timeCont.innerHTML = `${h}:${m}:${s}`;
+}, 1000);
